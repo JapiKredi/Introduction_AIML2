1c62cfb126d659cf496fc22acc1420d5f0494731
JapiKredi <jasper.bongers@yahoo.com>
2024-11-29 21:35:29 +0800

    commit

--- app.py
+++ app.py
@@ -12,3 +12,22 @@ from plotly.subplots import make_subplots
 
 st.title('Hello Streamlit')
 
+# Display a simple text
+st.write('This is a simple text')
+st.write('This is another text')
+
+# Create a simple dataframe
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+# Display the dataframe
+st.write("Here is the dataframe")
+st.write(df)
+
+# Create a line chart
+chart_datat = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
+st.line_chart(chart_datat)
+
