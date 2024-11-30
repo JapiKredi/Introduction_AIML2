07ba46a29d25445eb593d8440d35383317b8932e
JapiKredi <jasper.bongers@yahoo.com>
2024-11-30 12:38:56 +0800

    commit

--- 14.-Streamlit/classification.py
+++ 14.-Streamlit/classification.py
@@ -2,18 +2,15 @@ import streamlit as st
 import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-
 
 @st.cache
 def load_data():
-    iris = load_iris()
-    df = pd.DataFrame(iris.data, columns=iris.feature_names)
-    df['species'] = iris.target
-    return df, iris.target_names
+    data, target, feature_names, target_names = load_iris(return_X_y=True, return_feature_names=True, return_target_names=True)
+    df = pd.DataFrame(data, columns=feature_names)
+    df['species'] = target
+    return df, target_names
 
-
-df, target_names = load_data()
+df, target_name = load_data()
 
 print(df.head())
 st.write(df.head())
