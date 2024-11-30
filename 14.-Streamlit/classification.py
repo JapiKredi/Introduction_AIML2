a4f2c13917cecbcec7548bd2e8e29b777a61aa13
JapiKredi <jasper.bongers@yahoo.com>
2024-11-30 12:21:15 +0800

    commit

---
+++ 14.-Streamlit/classification.py
@@ -0,0 +1,7 @@
+import streamlit as st 
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+
